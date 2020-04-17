53a8e87043cbf3c005de7f879ff5c01d61bc702f
Alctrain <Alctrain@163.com>
2020-04-17 17:18:40 +0800

    4.17

--- app/src/main/java/com/alctrain/android/rateapp/MainActivity.java
+++ app/src/main/java/com/alctrain/android/rateapp/MainActivity.java
@@ -2,17 +2,24 @@ package com.alctrain.android.rateapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View;
 import android.widget.Toast;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText rmb;
     TextView show;
+    public final String tag="rate";
+    private float dollarrate=0.1f;
+    private float eurorate=0.2f;
+    private float wonrate=0.3f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,31 +28,49 @@ public class MainActivity extends AppCompatActivity {
 
         rmb=(EditText) findViewById(R.id.rmb);
         show=(TextView) findViewById(R.id.show_out);
+        //get the new rate from sp
+        SharedPreferences sp=getSharedPreferences("rate", Activity.MODE_PRIVATE);
+        dollarrate=sp.getFloat("dollar_rate",0.0f);
+        eurorate=sp.getFloat("euro_rate",0.0f);
+        wonrate=sp.getFloat("won_rate",0.0f);
     }
 
     public  void onclick(View btn){
-    String str=rmb.getText().toString();
-    float r=0;
-    if(str.length()>0){
-        r=Float.parseFloat(str);
-    }else {
+        String str=rmb.getText().toString();
+        float r=0;
+        if(str.length()>0){
+            r=Float.parseFloat(str);
+        }else {
             Toast.makeText(this,"请输入金额",Toast.LENGTH_SHORT).show();
-    }
-    if(btn.getId()==R.id.button_dollar){
-        float val=r*(1/6.7f);
-        show.setText(String.valueOf(val));
-    }else if(btn.getId()==R.id.button_euro){
-        float val=r*(1/11.0f);
-        show.setText(String.valueOf(val));
-    }else if(btn.getId()==R.id.button_won){
-        float val=r*500;
-        show.setText(String.valueOf(val));
-    }
+        }
+        if(btn.getId()==R.id.button_dollar){
+            show.setText(String.format("%.2f",r*dollarrate));
+        }else if(btn.getId()==R.id.button_euro){
+            show.setText(String.format("%.2f",r*eurorate));
+        }else if(btn.getId()==R.id.button_won){
+            show.setText(String.format("%.2f",r*wonrate));
+        }
 
     }
 
     public void openone(View btn){
         Intent hello=new Intent(this,SecondActivity.class);
-        startActivity(hello);
+        hello.putExtra("dollar_rate_key",dollarrate);
+        hello.putExtra("euro_rate_key",eurorate);
+        hello.putExtra("won_rate_key",wonrate);
+        Log.i(tag,"openone:dollarrate="+dollarrate);
+        Log.i(tag,"openone:eurorate="+eurorate);
+        Log.i(tag,"openone:wonrate="+wonrate);
+        startActivityForResult(hello,1);
+    }
+
+    protected void onActivityResult(int requestcode,int resultcode,Intent data){
+
+        Bundle bun=data.getExtras();
+        dollarrate=bun.getFloat("new_dollar_key",0.1f);
+        eurorate=bun.getFloat("new_euro_key",0.1f);
+        wonrate=bun.getFloat("new_won_key",0.1f);
+
+        super.onActivityResult(requestcode,resultcode,data);
     }
 }
--- app/src/main/java/com/alctrain/android/rateapp/SecondActivity.java
+++ app/src/main/java/com/alctrain/android/rateapp/SecondActivity.java
@@ -2,13 +2,51 @@ package com.alctrain.android.rateapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
+import android.util.Log;
 
 public class SecondActivity extends AppCompatActivity {
+
+
+     EditText dollartext;
+     EditText eurotext;
+     EditText wontext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Intent main=getIntent();
+        float dollar=main.getFloatExtra("dollar_rate_key",0.0f);
+        float euro=main.getFloatExtra("euro_rate_key",0.0f);
+        float won=main.getFloatExtra("won_rate_key",0.0f);
+        dollartext=(EditText) findViewById(R.id.dollar_rate);
+        eurotext=(EditText) findViewById(R.id.euro_rate);
+        wontext=(EditText) findViewById(R.id.won_rate);
+        dollartext.setText(String.valueOf(dollar));
+        eurotext.setText(String.valueOf(euro));
+        wontext.setText(String.valueOf(won));
+    }
+
+
+    public void save(View btn){
+        Log.i("cfg","save:rate");
+        float newdollar=Float.parseFloat(dollartext.getText().toString());
+        float neweuro=Float.parseFloat(eurotext.getText().toString());
+        float newwon=Float.parseFloat(wontext.getText().toString());
+        Intent first=getIntent();
+        Bundle ball=new Bundle();
+        ball.putFloat("new_dollar_key",newdollar);
+        ball.putFloat("new_euro_key",neweuro);
+        ball.putFloat("new_won_key",newwon);
+        first.putExtras(ball);
+        setResult(2,first);
+        finish();
     }
 }
