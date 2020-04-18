a88f37a08e4c548c4a2cbbbf9c7c794c59b7d6c9
Alctrain <Alctrain@163.com>
2020-04-18 14:53:55 +0800

    week7

--- app/src/main/java/com/alctrain/android/rateapp/MainActivity.java
+++ app/src/main/java/com/alctrain/android/rateapp/MainActivity.java
@@ -6,16 +6,28 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View;
 import android.widget.Toast;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements Runnable {
 
     EditText rmb;
     TextView show;
+    Handler handler;
     public final String tag="rate";
     private float dollarrate=0.1f;
     private float eurorate=0.2f;
@@ -33,6 +45,21 @@ public class MainActivity extends AppCompatActivity {
         dollarrate=sp.getFloat("dollar_rate",0.0f);
         eurorate=sp.getFloat("euro_rate",0.0f);
         wonrate=sp.getFloat("won_rate",0.0f);
+        //open zixiancheng
+        Thread t =new Thread(this);
+        t.start();
+        handler=new Handler(){
+            @Override
+            public void handleMessage(Message msg){
+                if(msg.what==5){
+                    String str=(String) msg.obj;
+                    Log.i(tag,str);
+                    show.setText(str);
+                }
+
+                super.handleMessage(msg);
+            }
+        };
     }
 
     public  void onclick(View btn){
@@ -78,5 +105,47 @@ public class MainActivity extends AppCompatActivity {
         ed.apply();
 
         super.onActivityResult(requestcode,resultcode,data);
+    }
+    public void run(){
+        Log.i("str","zixiancheng yijing qi yong");
+        for(int i=1;i<=3;i++){
+            Log.i(tag,"i="+i);
+            try{
+                Thread.sleep(2000);}
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        Message msg=handler.obtainMessage();
+        msg.what=5;
+        msg.obj="Hello from run()";
+        handler.sendMessage(msg);
+
+        //get data from url
+        try {
+            URL url=new URL("http://www.usd-cny.com/icbc.htm");
+            HttpURLConnection http=(HttpURLConnection) url.openConnection();
+            InputStream in=http.getInputStream();
+            String html=output(in);
+            Log.i(tag,html);
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private String output(InputStream inputstream) throws IOException {
+        final int bufferSize = 1024;
+        final char[] buffer = new char[bufferSize];
+        final StringBuilder out = new StringBuilder();
+        Reader in = new InputStreamReader(inputstream, "gb2312");
+        for(;;){
+            int rsz=in.read(buffer,0,buffer.length);
+            if(rsz<0)
+                break;
+            out.append(buffer,0,rsz);
+        }
+        return out.toString();
     }
 }
